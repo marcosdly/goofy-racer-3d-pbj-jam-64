8a6ed837d41b57b9038170b78de840e1e2e5e1fb
Marcos Bião <dev.marcosdly@gmail.com>
2026-03-17 04:08:28 -0300

    chore: update quick_audio addon with assetplus version

--- addons/quick_audio/Audio.gd
+++ addons/quick_audio/Audio.gd
@@ -32,35 +32,35 @@ func play_sound_3d(sound: AudioStream, autoplay := true) -> AudioStreamPlayer3D:
 
 func fade_in(audio_stream_player, seconds := 1.0, tween := create_tween()):
   if not (audio_stream_player is AudioStreamPlayer or audio_stream_player is AudioStreamPlayer2D or audio_stream_player is AudioStreamPlayer3D):
-	push_error("Non-AudioStreamPlayer[XD] provided to Audio.fade_in(...)")
-	return
+    push_error("Non-AudioStreamPlayer[XD] provided to Audio.fade_in(...)")
+    return
   tween.tween_method(func(x): audio_stream_player.volume_db = linear_to_db(x), db_to_linear(audio_stream_player.volume_db), 1.0, seconds)
 
 func fade_out(audio_stream_player, seconds := 1.0, tween := create_tween()):
   if not (audio_stream_player is AudioStreamPlayer or audio_stream_player is AudioStreamPlayer2D or audio_stream_player is AudioStreamPlayer3D):
-	push_error("Non-AudioStreamPlayer[XD] provided to Audio.fade_out(...)")
-	return
+    push_error("Non-AudioStreamPlayer[XD] provided to Audio.fade_out(...)")
+    return
   tween.tween_method(func(x): audio_stream_player.volume_db = linear_to_db(x), db_to_linear(audio_stream_player.volume_db), 0.0, seconds)
   tween.tween_callback(func(): audio_stream_player.stop(); audio_stream_player.queue_free())
 
 func cross_fade(audio_stream_player_out, audio_stream_player_in, seconds := 1.0, tween := create_tween()):
   if not (audio_stream_player_out is AudioStreamPlayer or audio_stream_player_out is AudioStreamPlayer2D or audio_stream_player_out is AudioStreamPlayer3D):
-	push_error("Non-AudioStreamPlayer[XD] provided to Audio.cross_fade(...) as audio_stream_player_out")
-	return
+    push_error("Non-AudioStreamPlayer[XD] provided to Audio.cross_fade(...) as audio_stream_player_out")
+    return
   if not (audio_stream_player_in is AudioStreamPlayer or audio_stream_player_in is AudioStreamPlayer2D or audio_stream_player_in is AudioStreamPlayer3D):
-	push_error("Non-AudioStreamPlayer[XD] provided to Audio.cross_fade(...) as audio_stream_player_in")
-	return
+    push_error("Non-AudioStreamPlayer[XD] provided to Audio.cross_fade(...) as audio_stream_player_in")
+    return
   fade_in(audio_stream_player_in, seconds, tween)
   fade_out(audio_stream_player_out, seconds, tween.parallel())
 
 func sequential_fade(audio_stream_player_out, audio_stream_player_in, out_seconds := 1.0, in_seconds := out_seconds, tween := create_tween(), empty_seconds := 0.0):
   if not (audio_stream_player_out is AudioStreamPlayer or audio_stream_player_out is AudioStreamPlayer2D or audio_stream_player_out is AudioStreamPlayer3D):
-	push_error("Non-AudioStreamPlayer[XD] provided to Audio.sequential_fade(...) as audio_stream_player_out")
-	return
+    push_error("Non-AudioStreamPlayer[XD] provided to Audio.sequential_fade(...) as audio_stream_player_out")
+    return
   if not (audio_stream_player_in is AudioStreamPlayer or audio_stream_player_in is AudioStreamPlayer2D or audio_stream_player_in is AudioStreamPlayer3D):
-	push_error("Non-AudioStreamPlayer[XD] provided to Audio.sequential_fade(...) as audio_stream_player_in")
-	return
+    push_error("Non-AudioStreamPlayer[XD] provided to Audio.sequential_fade(...) as audio_stream_player_in")
+    return
   fade_out(audio_stream_player_out, out_seconds, tween)
   if empty_seconds > 0.0:
-	tween.tween_interval(empty_seconds)
+    tween.tween_interval(empty_seconds)
   fade_in(audio_stream_player_in, in_seconds, tween)
