aff312215ce5d43490de55fcdad3110b67ba8adc
Marcos Bião <dev.marcosdly@gmail.com>
2026-03-17 04:14:09 -0300

    feat: sk_racing_cameras addon

---
+++ addons/sk_racing_cameras/plugin.gd
@@ -0,0 +1,12 @@
+@tool
+extends EditorPlugin
+
+const _shared := preload("res://addons/sk_racing_cameras/src/shared.gd")
+
+
+func _enter_tree() -> void:
+	add_autoload_singleton(_shared.SINGLETON_NAME, "res://addons/sk_racing_cameras/src/cameraman.gd")
+
+
+func _exit_tree() -> void:
+	remove_autoload_singleton(_shared.SINGLETON_NAME)
